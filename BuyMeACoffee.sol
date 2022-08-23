594b601987d0d6b6c868b2da9ef1112ae6be5118
HAPPYS1NGH <92804770+HAPPYS1NGH@users.noreply.github.com>
2022-08-23 23:24:02 +0530

    Create BuyMeACoffee.sol

---
+++ BuyMeACoffee.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier: Unlicense
+
+
+pragma solidity ^0.8.0;
+
+// BuyMeACoffee deployed to: 0x3e9C748E9DBB864Ee4dE65FA16343Cde878DF7D0
+
+contract BuyMeACoffee {
+    // Event to emit when a Memo is created.
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    // Memo struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    // Address of contract deployer. Marked payable so that
+    // we can withdraw to this address later.
+    address payable owner;
+
+    // List of all memos received from coffee purchases.
+    Memo[] memos;
+
+    constructor() {
+        // Store the address of the deployer as a payable address.
+        // When we withdraw funds, we'll withdraw here.
+        owner = payable(msg.sender);
+    }
+
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+
+    function buyCoffee(string memory _name, string memory _message)
+        public
+        payable
+    {
+        // Must accept more than 0 ETH for a coffee.
+        require(msg.value > 0, "can't buy coffee for free!");
+
+        // Add the memo to storage
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+
+        // Emit a NewMemo event with details about the memo.
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+
+  
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+}
